4315215d2c8f7a01a26070981a606c887a9b2765
Carlos Fabbri <carlosfg.97@gmail.com>
2018-02-21 15:29:02 -0500

    se crea la tabla de recomendaciones de productos para cada consultora con status de nueva en la campaña 10, segun el algoritmo cross selling

--- CrossSelling benchmark grafos.sql
+++ CrossSelling benchmark grafos.sql
@@ -6,6 +6,7 @@ select Izquierda, Derecha, Perfil, Izquierda+Derecha as tupla, Soporte, Confianz
 from BD_ANALITICO.DBO.MDL_AsociacionOutput
 where codpais = 'CL' AND AnioCampanaProceso = '201710' AND lift > 1 and confianza > 0.1
 order by izquierda, Perfil
+select * from #concat
 
 drop table #conteo
 select tupla, count(tupla) as counts into #conteo
@@ -24,19 +25,41 @@ into #RecomConScore
 inner join #concat b on a.tupla = b.tupla
 group by a.tupla
 
-drop table #RecomFinal
+drop table CF_RecomFinal
 select distinct b.Izquierda, b.Derecha, a.Soporte, a.Confianza, a.Lift 
-into #RecomFinal
+into CF_RecomFinal
 from #RecomConScore a
 inner join #concat b on a.tupla = b.tupla
 
-select * from #RecomFinal
-
-select * from DWH_ANALITICO.dbo.DWH_FVTAPROEBECAM
-where AnioCampana = '201710' and CodPais = 'CL'
+select * from CF_RecomFinal
 
 
+-----aca es para crucar con las consultoras. falta saber que codigo es el que muesra  el cross selling
+
+
+DROP TABLE #CUC
+select PkProducto, '{'+cast(CodCUC as varchar)+'}' as CUC, DesCategoria, DesSubCategoria into #CUC
+from  DWH_ANALITICO.dbo.DWH_DPRODUCTO 
+where CodPais = 'CL' 
+and DesCategoria in ('tratamiento corporal', 'tratamiento facial', 'cuidado personal', 'maquillaje', 'fragancias')
+and CodCUC <> '' and CodCUC is not null
+--(15765 row(s) affected)
 
 
 
--- BD_ANALITICO.DBO.MDL_PerfilOutput 
+
+drop table #PkNuevas
+select distinct PkEbelista  into #PkNuevas
+FROM [DWH_ANALITICO].[dbo].[DWH_FSTAEBECAM]
+where CodComportamientoRolling = 1 and CodPais = 'CL' and AnioCampana = '201710'
+
+
+drop table CF_RecomFinal_Consultoras
+select CodPais, AnioCampana,  a.PKEbelista, a.PKProducto, c.CUC as CUC_0, d.Derecha AS CUC_1 , c.DesCategoria, c.DesSubCategoria, RealUUVendidas, RealVtaMNNeto
+into CF_RecomFinal_Consultoras
+FROM DWH_ANALITICO.dbo.DWH_FVTAPROEBECAM a 
+INNER JOIN #PkNuevas b on a.PkEbelista = b.PkEbelista and CodPais = 'CL' and AnioCampana = '201710'
+inner join #CUC c on a.PkProducto = c.PKProducto and CodPais = 'CL' 
+inner join CF_RecomFinal d on c.CUC = d.Izquierda 
+
+select top (1000) * from CF_RecomFinal_Consultoras
